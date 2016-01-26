d275de00ac2b30d1fab499353425ce13a5cc7a90
Bart Siwek <bartlomiej.siwek@gmail.com>
2016-01-26 16:45:28 +0100

    Move to CMocka for testing

---
+++ cmake/cmocka.cmake
@@ -0,0 +1,30 @@
+# Enable ExternalProject CMake module
+include(ExternalProject)
+
+# We need git
+find_package(Git REQUIRED)
+
+# Download and install GoogleTest
+ExternalProject_Add(
+  cmocka
+  GIT_REPOSITORY git://git.cryptomilk.org/projects/cmocka.git
+  GIT_TAG cmocka-1.0.1
+  PREFIX ${CMAKE_CURRENT_BINARY_DIR}/cmocka
+  PATCH_COMMAND "${GIT_EXECUTABLE}" apply "${CMAKE_CURRENT_LIST_DIR}/cmocka.vs2015.patch"
+  INSTALL_COMMAND ""
+)
+
+# Get the binary and source dirs
+ExternalProject_Get_Property(cmocka source_dir binary_dir)
+
+# Create a libcmocka target to be used as a dependency by test programs
+add_library(libcmocka IMPORTED STATIC GLOBAL)
+add_dependencies(libcmocka cmocka)
+
+# Set libcmocka properties
+set_target_properties(libcmocka PROPERTIES
+  "IMPORTED_LOCATION" "${binary_dir}/src/${CMAKE_CFG_INTDIR}/cmocka.lib"
+  "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
+)
+include_directories("${source_dir}/include")
+
--- src/dxfw-tests/dxfw-tests.cpp
+++
@@ -1,3 +0,0 @@
-#include <dxfw/dxfw.h>
-
-#include <gtest/gtest.h>
