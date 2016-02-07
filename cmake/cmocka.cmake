96eb6f4071fe1aa3f62e1cde9f902ee29aaecbc9
Bart Siwek <bartlomiej.siwek@gmail.com>
2016-02-07 14:59:24 +0100

    Allow CMocka cmake extas to be included multiple times

--- cmake/cmocka.cmake
+++ cmake/cmocka.cmake
@@ -1,29 +1,33 @@
-# Enable ExternalProject CMake module
-include(ExternalProject)
+if(NOT DEFINED CMOCKA_CONFIGURED)
+	# Enable ExternalProject CMake module
+	include(ExternalProject)
 
-# We need git
-find_package(Git REQUIRED)
+	# We need git
+	find_package(Git REQUIRED)
 
-# Download and install Cmocka
-ExternalProject_Add(
-  cmocka
-  GIT_REPOSITORY git://git.cryptomilk.org/projects/cmocka.git
-  GIT_TAG cmocka-1.0.1
-  PREFIX ${CMAKE_CURRENT_BINARY_DIR}/cmocka
-  PATCH_COMMAND "${GIT_EXECUTABLE}" checkout . COMMAND "${GIT_EXECUTABLE}" apply "${CMAKE_CURRENT_LIST_DIR}/cmocka.vs2015.patch"
-  INSTALL_COMMAND ""
-)
+	# Download and install Cmocka
+	ExternalProject_Add(
+	  cmocka
+	  GIT_REPOSITORY git://git.cryptomilk.org/projects/cmocka.git
+	  GIT_TAG cmocka-1.0.1
+	  PREFIX ${CMAKE_CURRENT_BINARY_DIR}/cmocka
+	  PATCH_COMMAND "${GIT_EXECUTABLE}" checkout . COMMAND "${GIT_EXECUTABLE}" apply "${CMAKE_CURRENT_LIST_DIR}/cmocka.vs2015.patch"
+	  INSTALL_COMMAND ""
+	)
 
-# Get the binary and source dirs
-ExternalProject_Get_Property(cmocka source_dir binary_dir)
+	# Get the binary and source dirs
+	ExternalProject_Get_Property(cmocka source_dir binary_dir)
 
-# Create a libcmocka target to be used as a dependency by test programs
-add_library(libcmocka IMPORTED SHARED GLOBAL)
-add_dependencies(libcmocka cmocka)
+	# Create a libcmocka target to be used as a dependency by test programs
+	add_library(libcmocka IMPORTED SHARED GLOBAL)
+	add_dependencies(libcmocka cmocka)
 
-# Set libcmocka properties
-set_target_properties(libcmocka PROPERTIES
-  "IMPORTED_IMPLIB" "${binary_dir}/src/${CMAKE_CFG_INTDIR}/cmocka.lib"
-  "IMPORTED_LOCATION" "${binary_dir}/src/${CMAKE_CFG_INTDIR}/cmocka.dll"
-)
-include_directories("${source_dir}/include")
+	# Set libcmocka properties
+	set_target_properties(libcmocka PROPERTIES
+	  "IMPORTED_IMPLIB" "${binary_dir}/src/${CMAKE_CFG_INTDIR}/cmocka.lib"
+	  "IMPORTED_LOCATION" "${binary_dir}/src/${CMAKE_CFG_INTDIR}/cmocka.dll"
+	)
+	include_directories("${source_dir}/include")
+
+	set(CMOCKA_CONFIGURED TRUE)
+endif()
